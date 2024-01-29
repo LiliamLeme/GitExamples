4b07417648a7c608c5a15c7031bf8c2a622b3ae8
LiliamLeme <62876278+LiliamLeme@users.noreply.github.com>
2024-01-29 15:57:55 +0000

    Update restAPI_get_capacity.ps1

--- RestApiFabric/Capacity/restAPI_get_capacity.ps1
+++ RestApiFabric/Capacity/restAPI_get_capacity.ps1
@@ -1,13 +1,13 @@
 ﻿# Define your Azure subscription, resource group, and other parameters
-$subscriptionId = "1ca05c7d-712f-4bf6-bbae-866f523c7a7f"
+$subscriptionId = ""
 $resourceGroupName = "SQL-HA-RG-New"
-$dedicatedCapacityName = "Trial-lilem-microsoft-com-05-18-2023-13-59-UTC"
+$dedicatedCapacityName = ""
 $apiVersion = "2022-03-01-preview"
 
 # Authenticate using the Service Principal credentials
-$ClientID = '985134f5-4ec8-4d61-aa9d-6b0325e98ec9'
-$ClientSecret =  'FP98Q~z.cp0c8ukLSC1oTdIF4jF21KTxZYc5taFY'
-$tenantId = '72f988bf-86f1-41af-91ab-2d7cd011db47'
+$ClientID = ''
+$ClientSecret =  ''
+$tenantId = '
 $resource = 'https://management.azure.com/'
 $ServicePrincipalName = "app_fabric"
 
