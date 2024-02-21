c041a0cfea3cdc01c09b0355f8a48a331b218dcd
LiliamLeme <62876278+LiliamLeme@users.noreply.github.com>
2024-02-21 16:43:33 +0000

    Update dag.py

--- dags/dag.py
+++ dags/dag.py
@@ -11,12 +11,13 @@ with  DAG(dag_id='Generic_Transfer_Operator',
 
         load_upload_data=GenericTransfer(
                         task_id='load_upload_data' ,
-                        sql="select @@version",
-                        destination_table ="FactInternetSales_Consold"  ,
+                        sql=r"""        select* from datbricksOnelake.dbo.FactInternetSales_Consold;        """,
+                        destination_table =r"""datbricksOnelake.dbo.FactInternetSales_Consold;        """ ,
                         source_conn_id="sqllitle_FabricWH"  ,
                         destination_conn_id="sqllitle_FabricWH" ,
                         dag=dag
 
                     )
+
 
 load_upload_data
